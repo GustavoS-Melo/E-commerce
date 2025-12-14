c0f2b227f6be501db6a47e3597fc70cefda5b361
Gustavo <gustavo.souza.melo@hotmail.com>
2025-12-14 13:09:55 -0300

    creating cards section

---
+++ Game-Store/src/components/GameCard/index.jsx
@@ -0,0 +1,18 @@
+import './styles.scss';
+
+export default function GameCard({game}){
+    return(
+        <div className="game-card">
+            <div className="game-card_image">
+                <img src="{game.image}" alt="" />
+            </div>
+
+            <div className="game-card_content">
+                <h3>{game.title}</h3>
+                <span className="game-card_price">R$ {game.price}</span>
+
+                <button className="btn btn-primary">Comprar</button>
+            </div>
+        </div>
+    );
+}
